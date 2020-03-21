7bee9987b8241d0a3024f0ac609ee6ad488f01c5
HoJin9622 <kiss0104040@gmail.com>
2020-03-21 13:50:26 +0900

    Landing Page 만들기(1)

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -3,4 +3,6 @@ export const USER_SERVER = "/api/users";
 
 export const API_URL = "https://api.themoviedb.org/3/";
 
+export const API_KEY = "be935a38bcde877e7e766f591f9ff959";
+
 export const IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
